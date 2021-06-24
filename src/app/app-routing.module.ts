2a0cfa508aa4bf5938ce1222b4f255a4fbc890c0
Jose Almoguera <jalmoguera@bravent.net>
2021-06-24 15:30:00 +0200

    primer routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViajesListComponent } from './viajes-list/viajes-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'viajes' },
+  { path: 'viajes', component: ViajesListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TipoDeViajePipe } from './tipo-de-viaje.pipe';
+import { ViajesCardListComponent } from './viajes-list/viajes-card-list/viajes-card-list.component';
+import { ViajesFilterComponent } from './viajes-list/viajes-filter/viajes-filter.component';
+import { ViajesListComponent } from './viajes-list/viajes-list.component';
+import { ViajesTableListComponent } from './viajes-list/viajes-table-list/viajes-table-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViajesListComponent,
+    ViajesCardListComponent,
+    ViajesTableListComponent,
+    ViajesFilterComponent,
+    TipoDeViajePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
